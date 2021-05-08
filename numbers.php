6fb07e2c0c7272ad097328227ddf1b5b511dc22b
chivenice <chivenice29@gmail.com>
2021-05-08 23:43:22 +0900

    add numbers.php

---
+++ numbers.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   
+</head>
+<body>
+    <?php
+    $num = 10;
+    $num--;
+    echo $num; 
+      ?>
+<p>
+    <?php
+      $num = 10;
+      $num *=50;
+      echo $num;
+      
+    ?>
+
+</p>
+<p>
+    <?php
+     echo abs(-100);
+      
+    ?>
+
+</p>
+<p>
+    <?php
+      echo sqrt(144);
+      
+    ?>
+
+</p>
+<p>
+    <?php
+     echo max(2, 10);
+    ?>
+
+</p>
+<p>
+    <?php
+
+     echo floor(3.3);
+    ?>
+
+</p>
+</body>
+</html>
